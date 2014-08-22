1fdffadc6fe5f9902975e3f2f68a6d81b786479e
Anton Zaytsev <me@antonzaytsev.com>
2014-08-22 21:26:17 +0700

    added insta app for production

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,9 +1,8 @@
-ENV['CLIENT_ID'] = '910380fe0780405f9d51ababaf69dd32'
-ENV['CLIENT_SECRET'] = '372e8e4eea29468cbc533b3e389dcc66'
-ENV['WEBSITE_URL'] = 'http://localhost:3000'
-ENV['REDIRECT_URI'] = 'http://localhost:3000/oauth/signin'
+# ENV['CLIENT_ID'] = '910380fe0780405f9d51ababaf69dd32'
+# ENV['CLIENT_SECRET'] = '372e8e4eea29468cbc533b3e389dcc66'
+# ENV['REDIRECT_URI'] = 'http://localhost:3000/oauth/signin'
 
 Instagram.configure do |config|
-  config.client_id = ENV['CLIENT_ID']
-  config.client_secret = ENV['CLIENT_SECRET']
+  config.client_id = Rails.application.secrets.instagram_client_id
+  config.client_secret = Rails.application.secrets.instagram_client_secret
 end
