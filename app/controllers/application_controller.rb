84a5d2c3b3e7dadc7660ab71a667052dc016d93d
Anton Zaytsev <me@antonzaytsev.com>
2014-08-22 21:18:23 +0700

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_filter do
-    if Setting.g('instagram_access_token').blank?
-      redirect_to oauth_connect_path
-    end
-  end
 end
