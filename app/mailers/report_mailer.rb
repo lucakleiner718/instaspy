5b2f2d4c0faf1fa3d8fecfff8ef76519ae0e05d0
Anton Zaytsev <me@antonzaytsev.com>
2014-08-26 23:03:17 +0700

    fixed mailer

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,6 +3,8 @@ class ReportMailer < ActionMailer::Base
     csv_files.each do |tag_name, csv_string|
       attachments["#{tag_name}.csv"] = csv_string
     end
+    ends = ends.class.name == 'String' ? Time.parse(ends) : ends
+    starts = starts.class.name == 'String' ? Time.parse(starts) : starts
 #"rob@ladylux.com", bcc:
     mail to: 'me@antonzaytsev.com', subject: "Weekly InstaSpy report #{starts.strftime('%m/%d/%y')}-#{ends.strftime('%m/%d/%y')}"
   end
