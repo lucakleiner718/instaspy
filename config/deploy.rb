1ae7384aa820b68a058065602aaad242ea0b2b86
Anton Zaytsev <me@antonzaytsev.com>
2014-08-26 15:39:29 +0700

    puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :pty, true
 
 set :log_level, :info #:debug
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
@@ -40,8 +40,13 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+
+      within release_path do
+        invoke 'puma:restart'
+      end
     end
   end
 
@@ -51,6 +56,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
+      #   puma.restart
       #   execute :rake, 'cache:clear'
       # end
     end
