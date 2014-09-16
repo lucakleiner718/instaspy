f20a4191ee74549eb1b86da6cb0ea6cc12af4190
Anton Zaytsev <me@antonzaytsev.com>
2014-09-16 20:08:54 +0700

    optimized

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,20 +17,27 @@ class PagesController < ApplicationController
     amount_of_days = 10
 
     amount_of_days.times do |i|
-      cat = (amount_of_days-1-i).days.ago.strftime('%m/%d')
+      cat = i.days.ago.strftime('%m/%d')
       @xcategories << cat
       blank[cat] = 0
     end
+
+    @xcategories.reverse!
 
     @groups = {}
 
     Tag.where(show_graph: 1).each do |tag|
       @groups[tag.name] = blank.dup
-      amount_of_days.times do |i|
-        day = (amount_of_days-i).days.ago
-        @groups[tag.name][day.strftime('%m/%d')] =
-          tag.media.where('created_time >= ?', day.beginning_of_day).where('created_time <= ?', day.end_of_day).size
+      tag.media.where('created_time >= ?', 9.days.ago.beginning_of_day).pluck(:created_time).each do |row|
+        @groups[tag.name][row.strftime('%m/%d')] += 1
       end
+
+      # @groups[tag.name] = blank.dup
+      # amount_of_days.times do |i|
+      #   day = (amount_of_days-i).days.ago
+      #   @groups[tag.name][day.strftime('%m/%d')] =
+      #     tag.media.where('created_time >= ?', day.beginning_of_day).where('created_time <= ?', day.end_of_day).size
+      # end
 
     end
 
