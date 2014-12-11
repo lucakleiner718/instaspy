da3dea773ae512432ccd54da707a1c6469303595
Anton Zaytsev <me@antonzaytsev.com>
2014-12-11 23:49:17 +0600

    returned back whenever only for grabbing new media

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,32 +3,32 @@ every 1.minute do
   runner 'Media.recent_media'
 end
 
-every 10.minutes do
-  # runner 'UserWorker.perform_async'
-  runner 'User.update_worker'
-end
-
-every :tuesday, at: '4am' do
-  # runner 'ReportWorker.perform_async'
-  runner 'Media.report'
-end
-
-every :day, at: '5am' do
-  runner 'Media.delete_old'
-end
-
-every 3.hours do
-  runner 'TagChartWorker.spawn'
-end
-
-every 12.hours do
-  runner "FollowersReport.new('shopbop').get_new"
-end
-
-every :thursday, at: '4am' do
-  runner "FollowersReport.new('shopbop').send_weekly_report"
-end
-
-every 1.day, at: '12am' do
-  runner 'StatWorker.daily_stat'
-end
+# every 10.minutes do
+#   # runner 'UserWorker.perform_async'
+#   runner 'User.update_worker'
+# end
+#
+# every :tuesday, at: '4am' do
+#   # runner 'ReportWorker.perform_async'
+#   runner 'Media.report'
+# end
+#
+# every :day, at: '5am' do
+#   runner 'Media.delete_old'
+# end
+#
+# every 3.hours do
+#   runner 'TagChartWorker.spawn'
+# end
+#
+# every 12.hours do
+#   runner "FollowersReport.new('shopbop').get_new"
+# end
+#
+# every :thursday, at: '4am' do
+#   runner "FollowersReport.new('shopbop').send_weekly_report"
+# end
+#
+# every 1.day, at: '12am' do
+#   runner 'StatWorker.daily_stat'
+# end
