ff46526a593c9ca30985e229d442ef4f4e7141cf
Anton Zaytsev <me@antonzaytsev.com>
2015-04-04 15:40:46 +0600

    fix of account login

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -31,6 +31,12 @@ class OauthController < ApplicationController
 
     raise unless account
 
+    Instagram.configure do |config|
+      config.client_id = account.client_id
+      config.client_secret = account.client_secret
+      config.no_response_wrapper = true
+    end
+
     response = Instagram.get_access_token(params[:code], redirect_uri: account.redirect_uri)
 
     login = InstagramLogin.where(account_id: account.id, ig_id: response.user.id).first_or_initialize
