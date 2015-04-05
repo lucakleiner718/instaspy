c65a0577f39f84dc6703a67f9c3c25599de40b6b
Anton Zaytsev <me@antonzaytsev.com>
2015-04-05 18:29:03 +0600

    fixed sortable on tags listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,10 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    attrs = request.query_parameters
+    attrs[:sort] = column
+    attrs[:direction] = direction
+    link_to title, attrs, {:class => css_class}
   end
 
 end
