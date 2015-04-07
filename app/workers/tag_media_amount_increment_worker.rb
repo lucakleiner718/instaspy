af84b1783aa8cac062191f4d039cde7f2b63d76e
Anton Zaytsev <me@antonzaytsev.com>
2015-04-07 23:38:38 +0600

    process tag increment as critical

--- app/workers/tag_media_amount_increment_worker.rb
+++ app/workers/tag_media_amount_increment_worker.rb
@@ -1,6 +1,8 @@
 class TagMediaCounterWorker
   include Sidekiq::Worker
 
+  sidekiq_options queue: :critical
+
   def perform tag_id, action='+'
     connection = ActiveRecord::Base.connection
     connection.execute("update tags set media_count=media_count#{action.in?(['+', '-']) ? action : '+'}1 where id=#{tag_id.to_i}")
