b8b69813f6cde9379a22d0dad5a2d0449d01c1d3
Anton Zaytsev <me@antonzaytsev.com>
2015-05-13 17:28:03 +0600

    deleted test dir, as I use rspec

--- test/controllers/reports_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ReportsControllerTest < ActionController::TestCase
-  test "should get followers" do
-    get :followers
-    assert_response :success
-  end
-
-end
--- test/mailers/previews/report_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/report_mailer
-class ReportMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/report_mailer/weekly
-  def weekly
-    ReportMailer.weekly
-  end
-
-end
