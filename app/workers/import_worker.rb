84ff3bb6d22073c0c241993e242cae3d20f71d09
Anton Zaytsev <me@antonzaytsev.com>
2015-05-15 00:49:20 +0600

    added in import worker ability to queue

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -4,52 +4,56 @@ class ImportWorker
 
   include Sidekiq::Worker
 
-  def perform i
-    return false if File.exists?("tmp/cache/import/#{i}")
+  def perform nums
+    nums = [nums] if nums.is_a? Integer
 
-    ts = Time.now
-    begin
-      file = open("http://charts.socialroot.co/reports/users-pack/#{i}.csv")
-    rescue OpenURI::HTTPError => e
-      return false
-    end
-    data = CSV.parse file.read
-    header = data.shift # header
+    nums.each do |i|
+      next if File.exists?("tmp/cache/import/#{i}")
 
-    added = 0
-    exists = 0
+      ts = Time.now
+      begin
+        file = open("http://charts.socialroot.co/reports/users-pack/#{i}.csv")
+      rescue OpenURI::HTTPError => e
+        next
+      end
+      data = CSV.parse file.read
+      header = data.shift # header
 
-    cols = header
-    cols.slice!(0,2)
+      added = 0
+      exists = 0
 
-    users = User.scoped.in(insta_id: data.map{|r| r[1]}).to_a
+      cols = header
+      cols.slice!(0,2)
 
-    rows = {}
-    users.each {|row| rows[row.insta_id] = row}
+      users = User.scoped.in(insta_id: data.map{|r| r[1]}).to_a
 
-    data.each do |user_row|
-      # t1 = Time.now
-      user = rows[user_row[1].to_i]
-      if user
-        exists += 1
-      else
-        user = User.new(insta_id: user_row[1])
-        added += 1
+      rows = {}
+      users.each {|row| rows[row.insta_id] = row}
+
+      data.each do |user_row|
+        # t1 = Time.now
+        user = rows[user_row[1].to_i]
+        if user
+          exists += 1
+        else
+          user = User.new(insta_id: user_row[1])
+          added += 1
+        end
+
+        cols.each do |column|
+          user[column] = user_row[cols.index{|r| r==column}+2] if user[column].nil?
+        end
+        user.save if user.changed?
+
+        # puts "#{exists}/#{added} / Time: #{(Time.now - t1).round(2)}s"
       end
 
-      cols.each do |column|
-        user[column] = user_row[cols.index{|r| r==column}+2] if user[column].nil?
-      end
-      user.save if user.changed?
+      time = (Time.now - ts).round(2)
 
-      # puts "#{exists}/#{added} / Time: #{(Time.now - t1).round(2)}s"
+      File.write "tmp/cache/import/#{i}", time
+
+      puts "File: #{i} / #{exists}/#{added} / time: #{time}s"
     end
-
-    time = (Time.now - ts).round(2)
-
-    File.write "tmp/cache/import/#{i}", time
-
-    puts "File: #{i} / #{exists}/#{added} / time: #{time}s"
   end
 
   def self.spawn start=0
