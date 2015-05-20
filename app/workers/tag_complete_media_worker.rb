55d8d1a0fc38b8b4c8d7fee32f3a9844029e5cd2
Anton Zaytsev <me@antonzaytsev.com>
2015-05-20 19:14:28 +0600

    improved tag complete media worker

--- app/workers/tag_complete_media_worker.rb
+++ app/workers/tag_complete_media_worker.rb
@@ -1,14 +1,14 @@
 class TagCompleteMediaWorker
   include Sidekiq::Worker
 
-  def perform tag_id, offset=nil, created_from=nil, total_limit=nil
+  def perform tag_id, offset: nil, created_from: nil, total_limit: nil
     tag = Tag.find tag_id
     tag.recent_media offset: offset, created_from: created_from, total_limit: total_limit
   end
 
-  def self.spawn tag_id
-    30.times do |i|
-      TagCompleteMediaWorker.perform_async tag_id, (i.days.ago if i > 0), (i+1).days.ago, total_limit: 50_000
+  def self.spawn tag_id, days: 30
+    days.times do |i|
+      TagCompleteMediaWorker.perform_async tag_id, offset: (i.days.ago if i > 0), created_from: (i+1).days.ago, total_limit: 1_000_000
     end
   end
 end
