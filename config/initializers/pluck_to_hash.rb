72c0e12f151e1330a9726d3869afba338367e144
Anton Zaytsev <me@antonzaytsev.com>
2015-06-22 14:39:40 +0600

    fixed pluck_to_hash

--- config/initializers/pluck_to_hash.rb
+++ config/initializers/pluck_to_hash.rb
@@ -4,7 +4,7 @@ module PluckToHash
   module Mongoid
     module CriteriaMethods
       def pluck_to_hash *keys
-        only(*keys).to_a.inject({}) {|obj, r| keys.each{|key| obj[key] = r[key]}; obj}
+        only(*keys).to_a.map{|item| [item].inject({}) {|obj, r| keys.each{|key| obj[key] = r[key]}; obj} }
       end
     end
 
