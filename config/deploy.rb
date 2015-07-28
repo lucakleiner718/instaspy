087082842aa052be28dfe8fc0dfdcaf35787d3c3
Anton Zaytsev <me@antonzaytsev.com>
2015-07-28 22:43:47 +0600

    fixed god cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,41 +43,60 @@ after 'deploy:restart', 'puma:restart'
 set :god_pid, "#{shared_path}/tmp/pids/god.pid"
 
 namespace :god do
-
-  task :restart do
-    on roles(:app), in: :parallel do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          pid = capture(:cat, fetch(:god_pid))
-          execute :kill, pid, '> /dev/null' if pid
-          execute :bundle, :exec, "god -c config/procs.god --pid #{fetch :god_pid}"
-        end
-      end
-    end
+  def god_is_running
+    capture(:bundle, "exec god status > /dev/null 2>&1 || echo 'god not running'") != 'god not running'
   end
 
+  # Must be executed within SSHKit context
+  def config_file
+    "#{release_path}/config/god.rb"
+  end
+
+  # Must be executed within SSHKit context
+  def start_god
+    execute :bundle, "exec god -c #{config_file}"
+  end
+
+  desc "Start god and his processes"
   task :start do
-    on roles(:app), in: :parallel do
+    on roles(:web) do
       within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :bundle, :exec, "god -c config/procs.god --pid #{fetch :god_pid}"
+        with RAILS_ENV: fetch(:rails_env) do
+          start_god
         end
       end
     end
   end
 
+  desc "Terminate god and his processes"
   task :stop do
-    on roles(:app), in: :parallel do
+    on roles(:web) do
       within release_path do
-        with rails_env: fetch(:rails_env) do
-          pid = capture(:cat, fetch(:god_pid))
-          execute :kill, pid, '> /dev/null' if pid
+        if god_is_running
+          execute :bundle, "exec god terminate"
         end
       end
     end
   end
 
+  desc "Restart god's child processes"
+  task :restart do
+    on roles(:web) do
+      within release_path do
+        with RAILS_ENV: fetch(:rails_env) do
+          if god_is_running
+            execute :bundle, "exec god load #{config_file}"
+            execute :bundle, "exec god restart"
+          else
+            start_god
+          end
+        end
+      end
+    end
+  end
 end
+
+after "deploy:updated", "god:restart"
 
 after 'deploy:publishing', 'god:restart'
 
