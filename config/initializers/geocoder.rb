5688ad2639bf79793cffbf3450513660421e62cb
Anton Zaytsev <me@antonzaytsev.com>
2015-07-29 15:49:33 +0600

    improved geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,7 @@
 Geocoder.configure(
   timeout:      10,
-  lookup:       :bing,
+  lookup:       :yandex,
+
   bing: {
     api_key: 'AreNey4-B-i5NhpxcZJg3hPEl_Gta4qp2vTlShk5Bc4qt2FXq9vGlgosts-Dzq1l',
   },
@@ -10,25 +11,12 @@ Geocoder.configure(
   opencagedata: {
     api_key: 'f1fa119e5e9651ccfb690481681f143c'
   },
-  #
-  # Geocoder::Configuration.api_key = 'd5dd99546055d0d5d6be0de04446595dd5bb365'
   language:     :en,
-  # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
-  # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
-  # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
-  # :api_key      => nil,         # API key for geocoding service
-  # :cache        => nil,         # cache object (must respond to #[], #[]=, and #keys)
-  # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
+  use_https: true,
 
   # exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and TimeoutError
   # :always_raise => [],
-  :always_raise => :all,
-
-  # calculation options
-  # :units     => :mi,       # :km for kilometers or :mi for miles
-  # :distances => :linear    # :spherical or :linear
-
-  # cache: Redis.new
+  :always_raise => :all
 )
