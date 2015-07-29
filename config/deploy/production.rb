1c4462ebd8e14d0d98a3829426a8b176d4f4108e
Anton Zaytsev <me@antonzaytsev.com>
2015-07-29 12:26:39 +0600

    fixed server address in deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ set :stage, :production
 # role :web, %w{54.148.163.62}
 # role :db, %w{54.148.163.62}
 
-server '52.7.75.240', user: 'app', roles: %w{web app db}
+server 'socialrootdata.com', user: 'app', roles: %w{web app db}
 
 set :filter, :roles => %w{app web db}
 
