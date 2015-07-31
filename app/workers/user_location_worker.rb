454d2b5d8b3f30cb0ce4cf47d5856fb0475c38dd
Anton Zaytsev <me@antonzaytsev.com>
2015-07-31 09:15:11 +0600

    user location worker improve

--- app/workers/user_location_worker.rb
+++ app/workers/user_location_worker.rb
@@ -5,14 +5,11 @@ class UserLocationWorker
   sidekiq_options queue: :low, retry: 3, unique: true, unique_args: -> (args) { [ args.first ] }
 
   def perform user_id
-    user = User.find(user_id)
-    user.update_location!
+    user = User.find(user_id) rescue ActiveRecord::RecordNotFound nil
+    user.update_location! if user
   end
 
   def self.spawn
-
-    # users = User.where('followed_by is null OR followed_by > 1000').where('location_updated_at is null OR location_updated_at < ?', 3.months.ago).limit(10).pluck(:id)
-
     User.where('followed_by is null OR followed_by > 1000')
       .where('location_updated_at is null OR location_updated_at < ?', 3.months.ago)
       .select(:id).find_in_batches(batch_size: 1000) do |users_group|
