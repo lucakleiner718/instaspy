da8d2717a9c17791a222606455421cfe8e447799
Anton Zaytsev <me@antonzaytsev.com>
2015-08-03 00:58:38 +0600

    improved users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def index
     @users = User.order(created_at: :desc).page(params[:page]).per(20)
+
+    @followees = {}
+    Follower.where(follower_id: @users.map(&:id)).each do |f|
+      @followees[f.follower_id] ||= 0
+      @followees[f.follower_id] += 1
+    end
+
+    @followers = {}
+    Follower.where(user_id: @users.map(&:id)).each do |f|
+      @followees[f.user_id] ||= 0
+      @followees[f.user_id] += 1
+    end
   end
 
   def show
