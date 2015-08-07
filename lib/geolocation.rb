8496da8e8b86af1303443e2f0b6772ab9a3c87f4
Anton Zaytsev <me@antonzaytsev.com>
2015-08-07 14:45:40 +0600

    hotfix of geolocation

--- lib/geolocation.rb
+++ lib/geolocation.rb
@@ -43,6 +43,7 @@ class Geolocation
       end
 
       @row = resp.first
+      raise Exception.new("Wrong class name #{@row.class.name.downcase}") unless @row.class.name.downcase.match(/::(\w+)$/)
       method_name = "process_#{@row.class.name.downcase.match(/::(\w+)$/)[1]}".to_sym
 
       break if @row
