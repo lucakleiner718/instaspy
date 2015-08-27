7e571678b6cd21004d6e4d4e5f4b97260da429c9
Anton Zaytsev <me@antonzaytsev.com>
2015-08-28 00:17:27 +0600

    hotifx

--- app/workers/user_followers_worker.rb
+++ app/workers/user_followers_worker.rb
@@ -1,6 +1,7 @@
 class UserFollowersWorker
 
-  include Sidekiq::Worker, unique: true, unique_args: -> (args) {
+  include Sidekiq::Worker
+  sidekiq_options unique: true, unique_args: -> (args) {
       if args[1] && args[1][:start_cursor]
         args
       else
