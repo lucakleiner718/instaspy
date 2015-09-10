4b20377f46c30d318ca9f9b2e1c89df6aa43ad98
Anton Zaytsev <me@antonzaytsev.com>
2015-09-10 23:41:53 +0600

    improved collect followers worker

--- app/workers/user_followers_worker.rb
+++ app/workers/user_followers_worker.rb
@@ -2,7 +2,7 @@ class UserFollowersWorker
 
   include Sidekiq::Worker
   sidekiq_options unique: true, unique_args: -> (args) {
-      if args[1] && (args[1][:start_cursor] || args[1]['start_cursor']) || args[1][:ignore_uniqueness]
+      if args[1] && (args[1][:start_cursor] || args[1]['start_cursor'] || args[1][:finish_cursor] || args[1]['finish_cursor'] || args[1][:ignore_uniqueness] || args[1]['ignore_uniqueness'])
         args
       else
         [ args.first ]
