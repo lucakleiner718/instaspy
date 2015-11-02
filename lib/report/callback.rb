416e89e3f86595999cf30489bb941887081e8cba
Anton Zaytsev <me@antonzaytsev.com>
2015-11-02 08:47:06 +0600

    fixed on_complete batch callback

--- lib/report/callback.rb
+++ lib/report/callback.rb
@@ -6,7 +6,7 @@ class Report::Callback
     ReportProcessProgressWorker.perform_async options['report_id']
   end
 
-  def on_complete
+  def on_complete status, options
     if status.failures > 0
       Sidekiq::Batch.new(jid).invalidate_all
       Sidekiq::Batch.new(jid).status.delete
