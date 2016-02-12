c842d9e670aff8964ab02b310653699f6b80b017
Anton Zaytsev <me@antonzaytsev.com>
2016-02-12 15:48:33 +0600

    Improved invalidate_batches

--- lib/reporter.rb
+++ lib/reporter.rb
@@ -262,7 +262,7 @@ class Reporter
   def self.invalidate_batches
     Sidekiq::BatchSet.new.each do |status|
       Sidekiq::Batch.new(status.bid).invalidate_all rescue nil
-      Sidekiq::Batch.new(status.bid).status.delete rescue nil
+      status.delete rescue nil
     end
     Report.where(status: 'in_process').each do |report|
       report.batches.each do |name, bid|
